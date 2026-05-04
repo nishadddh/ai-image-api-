9c4b65082dc6dbc136a9277a20c4fcfe8940b215
Nishad Shajahan <91965048+nishadddh@users.noreply.github.com>
2026-05-04 23:43:08 +0530

    Fix API_URL assignment in main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-API_URL = "API_URL = "https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo""
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo"
 HF_TOKEN = os.getenv("HF_TOKEN")  # secure token
 
 @app.get("/")
